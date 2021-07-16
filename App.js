470d4a8282267aa97d527a9df469290d1c76807f
federicocotogno <federicosou@fastmail.se>
2021-07-16 09:42:55 +0200

    Sample Switch project

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Switch } from 'react-native';
+
 
 export default function App() {
+  const [isEnabled, setIsEnabled] = useState(true);
+  const [text, setText] = useState('Press the switch!');
+
+
+  const toggleSwitch = () => {
+
+    if (isEnabled) {
+      setText('Inactive');
+    } else {
+      setText('Active');
+    }
+
+    setIsEnabled(previousState => !previousState);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ margin: 20, fontWeight: 'bold' }}>{text}</Text>
+      <Switch
+        trackColor={{ false: '#767577', true: 'tomato' }}
+        thumbColor={isEnabled ? '#f4f3f4' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
       <StatusBar style="auto" />
     </View>
   );
